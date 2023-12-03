f3b76c41d9977358b818e4e777bfddce45ac877d
gaurav <mail2gauravprajapati@gmail.com>
2023-12-03 16:45:21 +0530

    Renamed sfa.c --> main.c

--- src/main.c
+++ src/main.c
@@ -1,71 +1,96 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-
-#include "read_dict.h"
-#include "admin.h"
 #include "trie.h"
+#include "ui.h"
 
-extern tnode *DICTIONARY;
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-int main() {
+/***********************************************
+|  * .-. .-. .-. .   .   .-. . . .-. .-. . .   |
+|  * `-. |-' |-  |   |   |   |-| |-  |   |<    |
+|  * `-' '   `-' `-' `-' `-' ' ` `-' `-' ' `   |
+|  * Spelling Checker Application              |
+|----------------------------------------------|
+|  * @author Gaurav Prajapati   11             |
+|  * @author Sourabh Bhosale    48             |
+|  * @author Vivaswan Sapre     57             |
+|----------------------------------------------|
+|  * SY BTech ECE                              |
+|  * Div. B - B3                               |
+|----------------------------------------------|
+|  * This is a C program to check correctness  |
+|  * of an input spelling.                     |
+|                                              |
+|  * It uses a trie data structure having      |
+|  * 27 subnodes for storing words.            |
+***********************************************/
 
-    int ex = 0;
-    char word[50];
+int main()
+{
 
-    printf("Loading tree...\n");
-    load_tree();
-    printf("Tree loaded successfully\n");
+    init_frames();
 
+    tnode *DICT = file_to_trie();
 
-    do {
-        if(!login_status())
-            printf("MENU\n  0. EXIT\n  1. ADD WORD\n  2. CHECK WORD\n  3. LOGIN\n>>> ");
-        else
-            printf("MENU\n  0. EXIT\n  1. ADD WORD\n  2. CHECK WORD\n  3. LOGOUT\n>>> ");
+    int opt = 0;
+    bool res = false;
 
-        scanf("%d", &ex);
-
-        switch (ex)
-        {
-        case 0:
-            printf("Exiting...\n");
-            break;
+    char buf[32],ch;
+    
+    // start alternative buffer
+    printf("\033[?1049h");
+    
+    do
+    {
+        // display frame for menu [0]
+        display_ui(0);
+        fflush(stdin);
+        scanf("%d", &opt);
+        // display selected option [opt]
+        display_ui(opt);
         
+        fflush(stdin);
+        
+        switch (opt)
+        {
         case 1:
-            if(login_status()) {
-                printf("Enter Word to add: ");
-                scanf("%s", word);
-
-                str_to_tree(word);
-            } else {
-                printf("Error 403: FORBIDDEN\n");
-            }
+            scanf("%s", buf);
+            tn_insert(buf, DICT);
+            if(check_word(buf,DICT)) res = true;
+            else res = false;
             break;
 
         case 2:
-            printf("Enter Word to Check: ");
-            scanf("%s", word);
-
-            if(checkspell(word)) {
-                printf("%s is spelled correct\n", word);
-            } else {
-                printf("%s is spelled wrogn\n", word);
-            }
+            scanf("%s", buf);
+            res = tn_delete(buf, DICT);
             break;
 
         case 3:
-            if(login_status()) logout();
-            else login();
+            scanf("%s", buf);
+            res = check_word(buf, DICT);
             break;
-        
+
+        case 4:
+            // get rid of leading \n in input buffer
+            while((ch = fgetc(stdin)) == '\n');
+            buf[0] = ch;
+            buf[1] = '\0';
+            scanf("%[^\n]", buf+1);
+            res = check_sentence(buf, DICT);
+            break;
+
         default:
-            printf("Invalid\n");
             break;
         }
-    }
-    while(ex);
+        // set message line
+        set_message(opt,res,buf);
 
-    return EXIT_SUCCESS;
+    } while (opt);
+    // close alternate buffer
+    printf("\033[?1049l");
+
+    // free up memory taken by trie
+    tn_free(&DICT);
+
+    return 0;
 }
